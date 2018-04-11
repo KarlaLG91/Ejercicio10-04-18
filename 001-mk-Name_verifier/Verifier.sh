45c0cff2e5f4fc449a4939e61967e800a39a9187
Karla91 <klg1219sh@gmail.com>
2018-04-11 11:03:06 -0500

    Se agregaron condicionales para evaluar la validez de los campos para achivos Fastq

--- 001-mk-Name_verifier/Verifier.sh
+++ 001-mk-Name_verifier/Verifier.sh
@@ -4,27 +4,33 @@
 set -x
 FILES=$1
 
-#for f in $FILES
-#do
-	#if [ -f == "?_S?_L00[1-8]_R[1-2]_001.{fastq.gz}"]
-	#then
-		#echo "True"
-	#else
-		#echo "False"
-	#fi
-#done
 FILE_NAME_WITHOUT_PATH=$(basename -- "$FILES")
-	echo $FILE_NAME_WITHOUT_PATH
+        echo $FILE_NAME_WITHOUT_PATH
+FASTQ_EXTENSION=`echo $FILE_NAME_WITHOUT_PATH | grep -c ".fastq.gz"`
 NUMBER_OF_FIELDS_IN_NAME=`echo $FILE_NAME_WITHOUT_PATH | tr "_" "\n" | wc -l`
-	echo "La variable numero de campos es $NUMBER_OF_FIELDS_IN_NAME"
+        echo "La variable numero de campos es $NUMBER_OF_FIELDS_IN_NAME"
 SAMPLE_NAME=`echo $FILE_NAME_WITHOUT_PATH | cut -d"_" -f1` 
 SAMPLE_NUMBER=`echo $FILE_NAME_WITHOUT_PATH | cut -d"_" -f2`
 LANE=`echo $FILE_NAME_WITHOUT_PATH | cut -d"_" -f3`
 READ=`echo $FILE_NAME_WITHOUT_PATH | cut -d"_" -f4`
-LAST_SEGMENT=`echo $FILE_NAME_WITHOUT_PATH | cut -d"_" -f5`
+LAST_SEGMENT=`echo $FILE_NAME_WITHOUT_PATH | cut -d"_" -f5 | cut -d "." -f1`
 
-#if [$SAMPLE_NUMBER == "S*" ] [ $READ == "R1" ] || [ $READ == "R2" ] | [ $LAST_SEGMENT == '001.fastq.gz']
-	#then
-		#echo "True"
-
+if [ $NUMBER_OF_FIELDS_IN_NAME != '5' ] 
+        then
+                echo "[ERROR] Tu archivo no tiene 5 campos en su nombre"
+elif [ $READ != 'R1' ] && [ $READ != 'R2' ]
+        then
+                echo "[ERROR] Tu archivo no cumple con convencion R1 o R2"
+elif [ $LANE != 'L001' ]
+        then
+                echo "[ERROR] Tu archivo no identifica los carriles"
+elif [ $LAST_SEGMENT != '001' ]
+        then
+                echo "[ERROR] Tu archivo no incluye el ultimo segmento 001"
+elif [ $FASTQ_EXTENSION != "1" ]
+        then
+                echo "[ERROR] LA extension del archivo no es la adecuada"
+        else
+                echo "Tu archivo cumple con las convenciones de Illumina"
+fi
 
